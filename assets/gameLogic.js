2f5fc0411883ca34b729baaa0eabbdf47d2f609a
Vaniri <foxifennec@gmail.com>
2019-11-22 23:26:41 -0500

    refactor timeOut logic

--- assets/gameLogic.js
+++ assets/gameLogic.js
@@ -10,52 +10,57 @@ class Round {
             { question: `3 + true == 4`, answers: ["false", "Nun", "undefined", "true"], correct: 3 },
             { question: `[] + {}`, answers: ["[object Object]", "0", "false", "array"], correct: 0 },
             { question: `[1,2,3] == [1,2,3]`, answers: ["Null", "array", "false", "true"], correct: 2 },
-            { question: `!![]`, answers: ["[]", "false", "array", "false"], correct: 1},
-            { question: `true + false`, answers: ["1", "truefalse", "undefined", "false"], correct: 0},
-            { question: `Array(16).join("wat" - 1) + " Batman!"`, answers: ["Batman!", "NaNNNaNNNaNNNaNNNaNNNaNNNaNNNaNNNaNNNaNNNaNNNaNNNaNNNaNNNaNN Batman!", "NaNN", "undefined"], correct: 1, image: "" }
+            { question: `!![]`, answers: ["[]", "false", "array", "false"], correct: 1 },
+            { question: `true + false`, answers: ["1", "truefalse", "undefined", "false"], correct: 0 },
+            { question: `true + 4`, answers: ["4", "true4", "null", "5"], correct: 3 },
+            { question: `1 + 1 + "1"`, answers: ["21", "null", "111", "3"], correct: 0 }
         ];
         this.answerResponses = ["Nice job!", "Well Done!", "You get it!", "That's right!", "You are really a ninja!"];
         this.correctAnswers = 0;
         this.wrongAnswers = 0;
-        this.rounds = 10;
+        this.rounds = 12;
+        this.timer = null;
     }
 
     startGame() {
+        if (this.timer) { this.timer.clear(); }
         let questionBox = $('#question-box');
         let answersBox = $('#answers-table');
         answersBox.empty();
 
         if (!this.rounds) {
-            clearInterval(intervalHandle);
+            $('#choose-answer').text("");
             questionBox.text("The Game is over");
             answersBox.text(`Correct answers: ${this.correctAnswers} Wrong Answers: ${this.wrongAnswers}`);
             return;
         }
         $('#timer-box').text("");
-        let question = this.questions[Math.floor(Math.random() * this.questions.length)];
-
-        $('#text').text("choose you answer ");
-        this.questions = this.questions.filter(other => other !== question);
+        let curQuestion = this.questions[Math.floor(Math.random() * this.questions.length)];
+        
+        $('#text').text("");
+        $('#choose-answer').text("Choose your answer:");
+        this.questions = this.questions.filter(other => other !== curQuestion);
         this.rounds--;
 
-        questionBox.text(question.question);
-        countDown();
-        let tm = setTimeout(() => this.handleTimeOut(question), 5000);
+        questionBox.html(`<code>${curQuestion.question}</code>`);
+        this.timer = new TimeOut(5, () => this.handleTimeOut(curQuestion));
 
-        for (let i = 0; i < question.answers.length; i++) {
-            let answers = $(`<li class="answers"></li>`);
-            answers.appendTo(answersBox);
-            answers.text(question.answers[i]);
-            answers.click(() => {
-                clearTimeout(tm);
-                clearInterval(intervalHandle);
-                if (i === question.correct) {
+        for (let i = 0; i < curQuestion.answers.length; i++) {
+            let answer = $(`<li></li>`);
+            answer.appendTo(answersBox);
+            answer.html(`<code>${curQuestion.answers[i]}</code>`);
+            answer.click(() => {
+                this.timer.clear();
+                $('#choose-answer').text("");
+                if (i === curQuestion.correct) {
                     let answerResponse = this.answerResponses[Math.floor(Math.random() * this.answerResponses.length)];
                     questionBox.text(answerResponse);
                     this.correctAnswers++;
+                    answer.addClass('correct');
                 } else {
-                    questionBox.text(`You select wrong answer! Correct answer is: ${question.answers[question.correct]}`);
+                    questionBox.text(`You select wrong answer! Correct answer is: ${curQuestion.answers[curQuestion.correct]}`);
                     this.wrongAnswers++;
+                    answer.addClass('wrong');
                 }
                 setTimeout(() => this.startGame(), 2000);
             });
@@ -63,37 +68,40 @@ class Round {
 
     }
 
-    handleTimeOut(question) {
-        $('#question-box').text(`Correct answer is: ${question.answers[question.correct]}`);
+    handleTimeOut(curQuestion) {
+        $('#choose-answer').text("");
+        $('#question-box').text(`Correct answer is: ${curQuestion.answers[curQuestion.correct]}`);
         this.wrongAnswers++;
-        clearInterval(intervalHandle);
         setTimeout(() => this.startGame(), 1000);
     }
+}
 
+class TimeOut {
+    constructor(remainderTime, func) {
+        this.remainderTime = remainderTime + 1;
+        let timerDecrement = () => {
+            this.remainderTime--;
+            $('#timer-box').text(`${this.remainderTime}`);
+            if (this.remainderTime <= 0) {
+                $('#text').text("Sorry! The time is out!");
+                func();
+                this.clear();
+            }
+        };
+        timerDecrement();
+        this.intervalHandle = setInterval(timerDecrement, 1000);
+    }
+
+    clear() {
+        clearInterval(this.intervalHandle);
+    }
 }
 
 window.onload = () => {
-    $('#text').text("Find out if you're really a JS ninja and can predict its weird behavior or just have some fun. And don't forget that array plus array equal to empty string.");
+    $('#text').html(`Find out if you're really a JS ninja and can predict its behavior. <br/> And don't forget that <code>[] + [] = ""</code>!`);
     $('#start-game').click(function () {
         $('#start-game').toggle();
         let round = new Round();
         setTimeout(() => round.startGame(), 1000);
     });
-}
-
-let intervalHandle = null;
-
-function countDown() {
-    let reminderTime = 6;
-    let func = () => {
-        reminderTime--;
-        $('#timer-box').text(`${reminderTime}`);
-        if (reminderTime <= 0) {
-            $('#text').text("Sorry! The time is out!");
-            clearInterval(interval);
-        }
-
-    }
-    func();
-    intervalHandle = setInterval(func, 1000);
 }
